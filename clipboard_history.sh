2e3aea3aafaa1a3f4b8e214c29f74169a6ca918f
Juan Pastas <pastasjuan@gmail.com>
2015-07-13 22:02:31 -0500

    Clipboard history. Main.sh.

--- clipboard_history.sh
+++ clipboard_history.sh
@@ -1,1 +1,4 @@
-# TODO install clipboard history
+# install clipboard history
+sudo add-apt-repository -y ppa:rickyrockrat/parcellite-appindicator
+sudo apt-get update
+sudo apt-get install -y parcellite
---
+++ main.sh
@@ -0,0 +1,8 @@
+bash install.sh
+bash install_2.sh
+bash nvm_subl.sh
+bash install_3.sh
+bash others.sh
+bash heroku.sh
+bash clipboard_history.sh
+bash video.sh
