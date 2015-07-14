1f23053aec24abae83a69686a3824c25fc03f906
Juan Pastas <pastasjuan@gmail.com>
2015-07-13 21:49:38 -0500

    Kazam, VLC.

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 Install what I use: Rails and Meteor related tools.
 
-Not ready yet.
+Kazam for screencasting.
+
+Almost ready.
+
+# TODO
+
+1. Unify code in one script
+1.1. add keys at start
+1.1. apt-get update once
+1. Rename files according installed package
--- others.sh
+++ others.sh
@@ -1,3 +1,8 @@
-# TODO install skype call recorder
-# TODO install kazaam
-# TODO install vlc
+# install kazam
+sudo add-apt-repository -y ppa:kazam-team/unstable-series
+sudo apt-get update
+sudo apt-get install -y kazam python3-cairo python3-xlib
+
+# install vlc
+# did not work on chromebook
+sudo apt-get install -y vlc browser-plugin-vlc
