8c469d01b7bcf552ed3d1e861f764508f965aea2
Juan Pastas <pastasjuan@gmail.com>
2015-07-18 12:03:21 -0500

    While PR is accepted.

---
+++ meteor_aliases.sh
@@ -0,0 +1,1 @@
+wget -O ~/.oh-my-zsh/custom/plugins/meteor/meteor.plugin.zsh https://raw.githubusercontent.com/juanpastas/oh-my-zsh/5a452a8d853f0f46d8586ec9eeb2a0de89f1b1aa/plugins/meteor/meteor.plugin.zsh
