e6d78257512256f9faddbddd3f9cbd5c8667ffc1
Juan David Pastás Rivera <juandavid1707@gmail.com>
2015-07-22 10:59:44 -0500

    pbcopy and cordova.

---
+++ cordova.sh
@@ -0,0 +1,5 @@
+sudo apt-add-repository -y ppa:cordova-ubuntu/ppa
+sudo apt-get update
+sudo apt-get install -y cordova-cli
+npm install -g cordova
+
---
+++ pbcopy.sh
@@ -0,0 +1,3 @@
+source include/write_once.sh
+write_once "alias pbcopy='xclip -selection clipboard'" ~/.zshrc
+write_once "alias pbpaste='xclip -selection clipboard -o'" ~/.zshrc
